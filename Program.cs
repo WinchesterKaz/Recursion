6ff25f7218a63df7bf0351285f1a46f53e9e3979
Alena <alena.supernatural666@gmail.com>
2024-01-08 20:42:50 +0300

    .\Program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+ //Использовать рекурсию, не использовать циклы.
+ 
+ Console.Write("Введите число M: "); 
+int m = Convert.ToInt32(Console.ReadLine()); 
+ 
+Console.Write("Введите число N: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+ 
+SumFromMToN(m, n); 
+ 
+// вызов функции "сумма чисел от M до N" 
+void SumFromMToN(int m, int n) 
+{ 
+ Console.Write(SumMN(m - 1, n)); 
+} 
+ 
+// функция сумма чисел от M до N 
+int SumMN(int m, int n) 
+{ 
+ int res = m; 
+ if (m == n) 
+ return 0; 
+ else 
+ { 
+ m++; 
+ res = m + SumMN(m, n); 
+ return res; 
+ } 
+}
