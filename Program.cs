ad24bc9c15fc766cf5b6d87fa29240cae9c9cded
Alena <alena.supernatural666@gmail.com>
2024-01-08 20:53:08 +0300

    project3

--- Program.cs
+++ Program.cs
@@ -30,34 +30,57 @@
 //}
 //================================================================================================
 //Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-Console.Write("Введите число M: "); 
-int m = Convert.ToInt32(Console.ReadLine()); 
+//Console.Write("Введите число M: "); 
+//int m = Convert.ToInt32(Console.ReadLine()); 
  
-Console.Write("Введите число N: "); 
-int n = Convert.ToInt32(Console.ReadLine()); 
+//Console.Write("Введите число N: "); 
+//int n = Convert.ToInt32(Console.ReadLine()); 
  
-AkkermanFunction(m, n); 
+//AkkermanFunction(m, n); 
  
  
 // вызов функции Аккермана 
-void AkkermanFunction(int m, int n) 
+//void AkkermanFunction(int m, int n) 
 { 
     Console.Write(Akkerman(m, n)); 
 } 
  
 // функция Аккермана 
-int Akkerman(int m, int n) 
+//int Akkerman(int m, int n) 
+//{ 
+    //if (m == 0) 
+    //{ 
+        //return n + 1; 
+    //} 
+    //else if (n == 0 && m > 0) 
+    //{ 
+        //return Akkerman(m - 1, 1); 
+    //} 
+    //else 
+    //{ 
+        //return (Akkerman(m - 1, Akkerman(m, n - 1))); 
+    //} 
+//}
+//==================================================================================
+ //Задайте произвольный массив.  
+// Выведете его элементы, начиная с конца.  
+// Использовать рекурсию, не использовать циклы. 
+class MainReturn 
 { 
-    if (m == 0) 
+static void Main(string[] args) 
     { 
-        return n + 1; 
+        int[] array = { 1, 2, 5, 0, 10, 34 }; 
+ 
+        PrintArrayReversed(array, array.Length - 1); 
     } 
-    else if (n == 0 && m > 0) 
+ 
+    static void PrintArrayReversed(int[] arr, int index) 
     { 
-        return Akkerman(m - 1, 1); 
-    } 
-    else 
-    { 
-        return (Akkerman(m - 1, Akkerman(m, n - 1))); 
+        if (index >= 0) 
+        { 
+            Console.Write(arr[index] + " " ); 
+            PrintArrayReversed(arr, index - 1); 
+        } 
+  
     } 
 }
