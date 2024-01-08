4d8557fab60cb06922029e2580bc57f7635ed11f
Alena <alena.supernatural666@gmail.com>
2024-01-08 20:47:46 +0300

    project2

--- Program.cs
+++ Program.cs
@@ -1,30 +1,63 @@
 ﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
  //Использовать рекурсию, не использовать циклы.
+
+ //Console.Write("Введите число M: "); 
+//int m = Convert.ToInt32(Console.ReadLine()); 
  
- Console.Write("Введите число M: "); 
+//Console.Write("Введите число N: "); 
+//int n = Convert.ToInt32(Console.ReadLine()); 
+ 
+//SumFromMToN(m, n); 
+ 
+// вызов функции "сумма чисел от M до N" 
+//void SumFromMToN(int m, int n) 
+//{ 
+ //Console.Write(SumMN(m - 1, n)); 
+//} 
+ 
+// функция сумма чисел от M до N 
+//int SumMN(int m, int n) 
+//{ 
+ //int res = m; 
+ //if (m == n) 
+ //return 0; 
+ //else 
+ //{ 
+ //m++; 
+ //res = m + SumMN(m, n); 
+ //return res; 
+ //} 
+//}
+//================================================================================================
+//Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Console.Write("Введите число M: "); 
 int m = Convert.ToInt32(Console.ReadLine()); 
  
 Console.Write("Введите число N: "); 
 int n = Convert.ToInt32(Console.ReadLine()); 
  
-SumFromMToN(m, n); 
+AkkermanFunction(m, n); 
  
-// вызов функции "сумма чисел от M до N" 
-void SumFromMToN(int m, int n) 
+ 
+// вызов функции Аккермана 
+void AkkermanFunction(int m, int n) 
 { 
- Console.Write(SumMN(m - 1, n)); 
+    Console.Write(Akkerman(m, n)); 
 } 
  
-// функция сумма чисел от M до N 
-int SumMN(int m, int n) 
+// функция Аккермана 
+int Akkerman(int m, int n) 
 { 
- int res = m; 
- if (m == n) 
- return 0; 
- else 
- { 
- m++; 
- res = m + SumMN(m, n); 
- return res; 
- } 
+    if (m == 0) 
+    { 
+        return n + 1; 
+    } 
+    else if (n == 0 && m > 0) 
+    { 
+        return Akkerman(m - 1, 1); 
+    } 
+    else 
+    { 
+        return (Akkerman(m - 1, Akkerman(m, n - 1))); 
+    } 
 }
